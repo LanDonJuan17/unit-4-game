979ca9710a994809229a0923fe8e75e71a37fb94
Landon Evans <evans.landon@gmail.com>
2019-06-26 05:28:11 -0500

    Built and styled game, added audio

---
+++ assets/javascript/game.js
@@ -0,0 +1,199 @@
+var baseAttack = 0;
+var player;
+var defender;
+var charArray = [];
+var playerSelected = false;
+var defenderSelected = false;
+
+
+
+function Character(name, hp, ap, counter, pic) {
+    this.name = name;
+    this.healthPoints = hp;
+    this.attackPower = ap;
+    this.counterAttackPower = counter;
+    this.pic = pic;
+}
+
+
+
+Character.prototype.increaseAttack = function () {
+    this.attackPower += baseAttack;
+};
+
+
+Character.prototype.attack = function (Obj) {
+    Obj.healthPoints -= this.attackPower;
+    $("#msg").html("You attacked " +
+        Obj.name + "for " + this.attackPower + " damage ");
+    this.increaseAttack();
+};
+
+
+Character.prototype.counterAttack = function (Obj) {
+    Obj.healthPoints -= this.counterAttackPower;
+    $("#msg").append("<br>" + this.name + " counter attacked for " + this.counterAttackPower + " damage ");
+};
+
+
+
+function initCharacters() {
+    var yoda = new Character("Master Yoda", 100, 10, 5, "./assets/images/yoda.jpg");
+    var macewindu = new Character("Mace Windu", 200, 50, 30, "./assets/images/macewindu.jpg");
+    var anakin = new Character("Anakin Skywalker", 150, 15, 2, "./assets/images/anakin.jpg");
+    var darthmaul = new Character("Darth Maul", 180, 30, 12, "./assets/images/darthmaul.jpg");
+    charArray.push(yoda, macewindu, anakin, darthmaul);
+}
+
+
+function setBaseAttack(Obj) {
+    baseAttack = Obj.attackPower;
+}
+
+
+function isAlive(Obj) {
+    if (Obj.healthPoints > 0) {
+        return true;
+    }
+    return false;
+}
+
+
+function isWinner() {
+    if (charArray.length == 0 && player.healthPoints > 0)
+        return true;
+    else return false;
+}
+
+
+function characterCards(divID) {
+    $(divID).children().remove();
+    for (var i = 0; i < charArray.length; i++) {
+        $(divID).append("<div />");
+        $(divID + " div:last-child").addClass("card");
+        $(divID + " div:last-child").append("<img />");
+        $(divID + " img:last-child").attr("id", charArray[i].name);
+        $(divID + " img:last-child").attr("class", "card-img-top");
+        $(divID + " img:last-child").attr("src", charArray[i].pic);
+        $(divID + " img:last-child").attr("width", 150);
+        $(divID + " img:last-child").addClass("img-thumbnail");
+        $(divID + " div:last-child").append(charArray[i].name + "<br>");
+        $(divID + " div:last-child").append("HP: " + charArray[i].healthPoints);
+        $(divID + " idv:last-child").append();
+
+    }
+}
+
+
+function updatePics(fromDivID, toDivID) {
+    $(fromDivID).children().remove();
+    for (var i = 0; i < charArray.length; i++) {
+        $(toDivID).append("<img />");
+        $(toDivID + " img:last-child").attr("id", charArray[i].name);
+        $(toDivID + " img:last-child").attr("src", charArray[i].pic);
+        $(toDivID + " img:last-child").attr("width", 150);
+        $(toDivID + " img:last-child").addClass("img-thumbnail");
+    }
+}
+
+
+function playAudio() {
+    var audio = new Audio("assets/media/Star Wars Theme Song by John Williams.mp3");
+    audio.play();
+}
+
+
+
+
+function changeView() {
+    $("#firstScreen").empty();
+    $("#secondScreen").show();
+}
+
+
+$(document).on("click", "img", function () {
+    if (playerSelected && !defenderSelected && (this.id != player.name)) {
+        for (var j = 0; j < charArray.length; j++) {
+            if (charArray[j].name == (this).id) {
+                defender = charArray[j];
+                charArray.splice(j, 1);
+                defenderSelected = true;
+                $("#msg").html("Click the button to attack!");
+            }
+        }
+        $("#defenderDiv").append(this);
+        $("#defenderDiv").addClass("animated zoomInRight");
+        $("#defenderDiv").append("<br>" + defender.name);
+        $("#defenderHealthDiv").append("HP: " + defender.healthPoints);
+        $("#defenderHealthDiv").addClass("animated zoomInRight");
+    }
+
+    if (!playerSelected) {
+        for (var i = 0; i < charArray.length; i++) {
+            if (charArray[i].name == (this).id) {
+                player = charArray[i];
+                playAudio();
+                $("body").css({
+                    "background-image": "url('./assets/images/" + this.id[0] + ".jpg')"
+                });
+                setBaseAttack(player);
+                charArray.splice(i, 1);
+                playerSelected = true;
+                changeView();
+                $("#msg").html("Choose your battle wisely!");
+            }
+        }
+        updatePics("#game", "#defendersLeftDiv");
+        $("#playerDiv").append(this);
+        $("#playerDiv").addClass("animated zoomIn");
+        $("#playerDiv").append(player.name);
+        $("#playerHealthDiv").append("HP: " + player.healthPoints);
+        $("#playerHealthDiv").addClass("animated zoomIn");
+    }
+
+});
+
+//
+$(document).on("click", "#attackBtn", function () {
+    if (playerSelected && defenderSelected) {
+        if (isAlive(player) && isAlive(defender)) {
+            player.attack(defender);
+            defender.counterAttack(player);
+            $("#playerHealthDiv").html("HP: " + player.healthPoints);
+            $("#defenderHealthDiv").html("HP: " + defender.healthPoints);
+            if (!isAlive(defender)) {
+                $("#defenderHealthDiv").html("DEFEATED!");
+                $("#playerHealthDiv").html("Enemy defeated!");
+                $("#msg").html("Choose your next opponent!");
+            }
+            if (!isAlive(player)) {
+                $("#playerHealthDiv").html("The Empire has prevailed...");
+                $("#msg").html("The Force will be with you, always...try again...");
+                $("#attackBtn").html("Restart Game");
+                $(document).on("click", "#attackBtn", function () {
+                    location.reload();
+                });
+            }
+        }
+        if (!isAlive(defender)) {
+            $("#defenderDiv").removeClass("animated zoomInRight");
+            $("#defenderHealthDiv").removeClass("animated zoomInRight");
+            $("#defenderDiv").children().remove();
+            $("#defenderDiv").html("");
+            $("#defenderHealthDiv").html("");
+            defenderSelected = false;
+            if (isWinner()) {
+                $("#secondScreen").hide();
+                $("#globalMsg").show();
+            }
+        }
+    }
+});
+
+
+$(document).ready(function () {
+    $("#secondScreen").hide();
+    $("#globalMsg").hide();
+    initCharacters();
+    characterCards("#game");
+});
